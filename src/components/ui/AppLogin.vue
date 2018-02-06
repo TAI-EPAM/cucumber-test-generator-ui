19b4176c6fb082c3950118d064533401363894fd
D.Nekiy <Dmitry_Babanov@epam.com>
2018-02-06 07:06:58 +0300

    - Login Page Layout

--- src/components/ui/AppLogin.vue
+++ src/components/ui/AppLogin.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="uui-form-wrapper login-form">
-    <input type="text" name="login" value="" class="uui-form-element large" placeholder="E-mail" v-model="entity.email" />
-    <input type="password" name="password" value="" class="uui-form-element large" placeholder="Password" v-model="entity.password" />
-    <div class="form-buttons-holder">
-      <v-button @click="reset" class="large">Reset</v-button>
-      <v-button @click="login" class="lime-green large">Login</v-button>
+  <div class="wrapper" id="app">
+
+    <div class="splashWrapper">
+      <div class="splash"></div>
     </div>
+    <div class="contentWrapper">
+      <h1>Authorization</h1>
+      <div class="uui-form-wrapper login-form">
+        <input type="text" name="login" value="" class="uui-form-element large" placeholder="E-mail" v-model="entity.email" />
+        <input type="password" name="password" value="" class="uui-form-element large" placeholder="Password" v-model="entity.password" />
+        <div class="form-buttons-holder">
+          <v-button @click="reset" class="large">Reset</v-button>
+          <v-button @click="login" class="lime-green large">Login</v-button>
+        </div>
+      </div>
+    </div>
+
   </div>
+
+
+
 </template>
 
 <script>
@@ -62,7 +75,38 @@
 
 <style lang="less" scoped>
   .login-form {
-    width: 600px;
+    width: 400px;
     margin: 0 auto;
   }
+
+  .splashWrapper {
+    width: 100%;
+    height: 100%;
+    background: #1a9cb0;
+    float: left;
+
+    & > .splash {
+      height: 100%;
+      margin-right: 500px;
+      margin-left: 0;
+      background: url("../../assets/images/login.png") 0 0 no-repeat;
+      background-size: cover;
+    }
+
+  }
+
+  .contentWrapper {
+    position: relative;
+    width: 500px;
+    height: 100%;
+    margin-left: -500px;
+    float: left;
+    background: white;
+
+    & > h1 {
+      margin: 100px auto 40px;
+      text-align: center;
+    }
+  }
+
 </style>
