0ca107b575be134e97e4ff0410615b7b9a685b47
Marimunira <m.maschinskaya@mail.ru>
2018-02-08 18:02:05 +0300

    Added validation for case add

--- src/components/case/CaseAdd.vue
+++ src/components/case/CaseAdd.vue
@@ -1,8 +1,16 @@
 <template>
   <section class="entity-add">
     <div class="uui-form-wrapper">
-      <input type="text" v-model="entity.name" class="uui-form-element large" placeholder="Case Name" />
-      <input type="text" v-model="entity.description" class="uui-form-element large" placeholder="Case Description" />
+       <input type="text" v-model="entity.name"
+          class="uui-form-element large"
+          placeholder="Case Name"
+          @input="$v.entity.name.$touch()"
+          :class="{ 'error': $v.entity.name.$error }"/>
+      <input type="text" v-model="entity.description"
+           class="uui-form-element large"
+           placeholder="Case Description"
+           @input="$v.entity.description.$touch()"
+           :class="{ 'error': $v.entity.description.$error }"/>
       <tags-component v-model="entity.tags"></tags-component>
       <div class="priority-component">
         <div class="title">Priority:</div>
@@ -13,7 +21,10 @@
     </div>
     <div class="form-buttons-holder">
       <epam-button @click="resetData" class="large">Cancel</epam-button>
-      <epam-button @click="sendData" class="lime-green large">Add Case</epam-button>
+      <epam-button @click="sendData" 
+        class="uui-button large"
+        :class="buttonClass"
+        v-bind:disabled="$v.entity.$invalid">Add Suit</epam-button>
     </div>
   </section>
 </template>
@@ -22,6 +33,7 @@
   import EpamButton from '../ui/EpamButton';
   import EpamMultiswitch from '../ui/EpamMuiltswitch';
   import TagsComponent from '../ui/TagsInput';
+  import mapValidations from '../../validator';
 
   export default {
     components: {
@@ -48,6 +60,7 @@
         ],
       };
     },
+    ...mapValidations(),
     methods: {
       resetData() {
         Object.assign(this.entity, {
@@ -74,6 +87,14 @@
       },
     },
     mounted() {
+    },
+    computed: {
+      buttonClass() {
+        return {
+          disable: this.$v.entity.$invalid,
+          'lime-green': !this.$v.entity.$invalid,
+        };
+      },
     },
     name: 'CaseAdd',
     props: ['projectId', 'suitId', 'onCancel', 'onSubmit'],
