587f3f849fed33f9f18125d1eec11c65f7ac8dbb
Marimunira <m.maschinskaya@mail.ru>
2018-02-12 15:48:39 +0300

    Added types of steps

--- src/components/suggestion/SuggestionItem.vue
+++ src/components/suggestion/SuggestionItem.vue
@@ -1,5 +1,10 @@
 <template>
-<input type="text" class="uui-form-element" :placeholder="suggestion.content" :value="suggestion.content"/>
+<div class="item-wrapper">
+    <select class="select-type">
+        <option v-for="item in types" :selected="suggestion.type===item.text">{{item.text}}</option>
+    </select>
+    <input type="text" class="uui-form-element input-suggestion" :placeholder="suggestion.content" :value="suggestion.content"/>
+</div>
 </template>
 
 
@@ -9,7 +14,13 @@
     components: {
     },
     data() {
-      return {};
+      return {
+        types: [
+          { value: 1, text: 'GIVEN' },
+          { value: 2, text: 'WHEN' },
+          { value: 3, text: 'THEN' },
+        ],
+      };
     },
     name: 'SuggestionItem',
     props: ['suggestion'],
@@ -17,8 +28,17 @@
 </script>
 
 <style lang="less" scoped>
-.uui-form-element {
-    width: 100%;
+.input-suggestion {
+    width: 90%;
     color: black;
+    display: inline;
+}
+.select-type {
+  display: inline;
+  height: 35px;
+}
+.item-wrapper {
+  width: 100%;
+  
 }
 </style>
