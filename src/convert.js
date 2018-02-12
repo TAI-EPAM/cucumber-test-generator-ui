87dd2d5270a233fb393db0b9abda569cea772d2b
Marimunira <m.maschinskaya@mail.ru>
2018-02-12 16:53:52 +0300

    Fix steps

--- src/convert.js
+++ src/convert.js
@@ -1,27 +1,17 @@
-function parseAttributesFromStr(str) {
-  if (!str) {
-    return {
-      id: '',
-      rowNumber: '',
-      description: '',
-      type: '',
-    };
-  }
-  const result = str.match(/=(.*?)[,}]/ig).map(el => el.slice(1, -1)) || [];
-  return {
-    id: +result[0],
-    rowNumber: +result[1],
-    description: result[2],
-    type: result[3],
-  };
-}
 function convertSteps(commit) {
   const c = commit;
+  const emptyStep = {
+    id: '',
+    rowNumber: '',
+    description: '',
+    type: '',
+    comment: '',
+  };
   c.propertyDifferences = c.propertyDifferences.map((item) => {
     if (item.propertyName === 'steps') {
       const i = item;
-      i.oldValue = parseAttributesFromStr(item.oldValue);
-      i.newValue = parseAttributesFromStr(item.newValue);
+      i.oldValue = i.oldValue || emptyStep;
+      i.newValue = i.newValue || emptyStep;
       return i;
     }
     return item;
