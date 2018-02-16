7c4ad7bc534630e3c924c44a1c2a03b427d74416
Marimunira <m.maschinskaya@mail.ru>
2018-02-16 18:49:02 +0300

    Added constant for types of steps

--- src/components/suggestion/SuggestionItem.vue
+++ src/components/suggestion/SuggestionItem.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="item-wrapper">
     <select class="select-type">
-        <option v-for="item in types" :selected="suggestion.type===item.text">{{item.text}}</option>
+        <option v-for="item of types" :selected="suggestion.type===item[0]">{{item[1]}}</option>
     </select>
     <input type="text" class="uui-form-element input-suggestion" :placeholder="suggestion.content" :value="suggestion.content"/>
 </div>
@@ -9,17 +9,14 @@
 
 
 <script>
+  import StepTypeMap from '@/constants/StepType';
 
   export default {
     components: {
     },
     data() {
       return {
-        types: [
-          { value: 1, text: 'GIVEN' },
-          { value: 2, text: 'WHEN' },
-          { value: 3, text: 'THEN' },
-        ],
+        types: [...StepTypeMap],
       };
     },
     name: 'SuggestionItem',
