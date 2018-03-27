ea0b762135b09a34d1249a9a24a71724516eb3ed
anisa askarova <anisaaskarova@anisas-MBP.petersburg.epam.com>
2018-03-27 13:14:31 +0300

    edit case button logic, buttons look

--- src/components/case/CaseTopMenu.vue
+++ src/components/case/CaseTopMenu.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="caseTopPanel">
     <div class="caseName">Case {{this.localCase.name}}</div>
-    <div class="play"></div>
+    <div class="nextCase">
+      <a href="#" class="nextCaseButton"></a>
+    </div>
     <div class="history">
       <curtain
         buttonClass="historyButton"
@@ -9,10 +11,14 @@
         headerText="History"
         headerMarkup="orange"
         :component="getCertainComponent()"
-        v-if="localCase">>
+        v-if="localCase">
       </curtain>
     </div>
-    <div class="pen"></div>
+    <div class="editCase">
+      <a href="#" class="editCaseButton" @click.prevent="editCase">
+        <i class="fas fa-pencil-alt"></i>
+      </a>
+    </div>
   </section>
 </template>
 
@@ -21,17 +27,36 @@
   import { mapGetters } from 'vuex';
   import CaseHistory from '@/components/case/CaseHistory';
   import Curtain from '@/components/ui/Curtain';
+  import CaseEdit from '@/components/case/CaseEdit';
 
   export default {
     components: {
       CaseHistory,
       Curtain,
+      CaseEdit,
     },
     name: 'case-top-menu',
     data() {
       return {};
     },
     methods: {
+      editCase() {
+        this.$vuedals.open({
+          title: 'Edit Case',
+          component: CaseEdit,
+          props: {
+            value: this.localCase,
+            projectId: this.$route.params.projectId,
+            suitId: this.$route.params.suitId,
+            onCancel() {
+              this.$vuedals.close();
+            },
+            onSubmit() {
+              this.$vuedals.close();
+            },
+          },
+        });
+      },
       isCreatedCommit(commit) {
         const attributes = ['id', 'name', 'description', 'creationDate', 'updateDate', 'priority', 'status'];
         const oldValues = [];
@@ -84,23 +109,102 @@
       text-transform: capitalize;
     }
 
-    .play, .history, .pen {
+    .nextCase, .history, .editCase {
       width: 66px;
       height: 100%;
       border-left: 1px solid @gray_light;
       text-align: center;
-      padding-top: 15px;
+      position: relative;
     }
+
+    .linkBeforeMixin(@content) {
+      font-family: FontAwesome;
+      display: inline-block;
+      vertical-align: middle;
+      color: @gray;
+      font-size: 20px;
+      position: absolute;
+      top: 25px;
+      left: 25px;
+      content: @content;
+    }
+
+    .linkBeforeHoverMixin(@bg) {
+      height: 100%;
+      width: 100%;
+      display: inline-block;
+      background-color: @bg;
+    }
+
+    .editCaseButton:before {
+      .linkBeforeMixin("\f044");
+    }
+
+    .nextCaseButton:before {
+      .linkBeforeMixin("\f04e");
+    }
+
+    .nextCaseButton:hover {
+      .linkBeforeHoverMixin(@green_lime);
+    }
+
+    .editCaseButton:hover {
+      .linkBeforeHoverMixin(@fuchsia);
+    }
+
+    .editCaseButton:hover:before {
+      color: @fuchsia;
+    }
+
+    .nextCaseButton:hover:before {
+      color: @green_lime;
+    }
+
   }
 </style>
 
 <style lang="less">
+  @import "../../assets/vendors/epam-ui/less/uui-colors";
+  @import "../../assets/vendors/epam-ui/less/uui-fonts";
+
   button.uui-button.historyButton {
     background-color: transparent;
     border: none;
     font-size: 22px;
+    margin-top: 13px;
+    position: relative;
   }
   button.uui-button.historyButton:hover {
-    background-color: transparent;
+    background-color: @orange;
+    color: @orange;
+    width: 100%;
+    height: 68px;
+    margin: 0;
   }
+
+  .linkAfterMixin(@content) {
+    position: absolute;
+    color: @white;
+    font-size: 12px;
+    font-family: @Source_Sans_Pro_Bold;
+    line-height: 14px;
+    left: 7px;
+    right: 7px;
+    top: 14px;
+    text-transform: lowercase;
+    content: @content;
+  }
+
+  button.uui-button.historyButton:hover:after {
+    .linkAfterMixin('history of this case');
+  }
+
+  .nextCaseButton:hover:after {
+    .linkAfterMixin('save case and open next');
+  }
+
+  .editCaseButton:hover:after {
+    .linkAfterMixin('edit this case');
+  }
+
 </style>
