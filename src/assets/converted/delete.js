afa7eab484b3e45dc22825c553bb76e11d0f5bc1
Lesya <amix93@mail.ru>
2018-06-05 12:26:27 +0300

    add test run view

---
+++ src/assets/converted/delete.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import * as icon from 'vue-svgicon';
+icon.register({
+  'delete': {
+    width: 5,
+    height: 5,
+    viewBox: '0 0 14 18',
+    data: '<defs><style>.cls-1{fill:#ededed;}</style></defs><title>Ресурс 2</title><g id="Слой_2" data-name="Слой 2"><g id="Слой_1-2" data-name="Слой 1"><path class="cls-1" d="M14,1H10.5l-1-1h-5l-1,1H0V3H14ZM1,16a2,2,0,0,0,2,2h8a2,2,0,0,0,2-2V4H1Z"/></g></g></svg>'
+  }
+})
---
+++ src/components/ui/TestRun.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="test-run" >
+  <p>Test Run start</p>
+    <p>{{ this.$store.state.selectedObject }}}</p>
+  </section>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'test-run',
+    computed: {
+      ...mapGetters(
+        {
+          selectedObject: 'getSelectObject',
+        },
+      ),
+    },
+  };
+
+</script>
